da59b0f2201ec762474525cb613086582e0364fe
William Crews <crewswh@gmail.com>
2021-07-22 21:34:56 -0400

    node

--- controllers/config.js
+++ controllers/config.js
@@ -6,6 +6,18 @@ router.get('/', async (req, res) => {
 
 router.get('/home', async (req, res) => {
     res.render('home');
-  });
+});
+
+router.get('/life', async (req, res) => {
+  res.render('life');
+});
+
+router.get('/design', async (req, res) => {
+  res.render('design');
+});
+
+router.get('/company', async (req, res) => {
+  res.render('company');
+});
 
 module.exports = router;
